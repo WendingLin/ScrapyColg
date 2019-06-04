fa8b5e544893e1025f7a66e2159e8ff56db0641c
wending <wending.lin@unity3d.com>
2019-06-04 14:10:48 +0800

    A thought

--- scrcolg/scrcolg/spiders/test.py
+++ scrcolg/scrcolg/spiders/test.py
@@ -1,5 +1,7 @@
+import scrapy
 from scrapy.spiders import Spider
-from ..items import ScrcolgItem
+
+from ..items import topic, reply
 
 class DjSpider(Spider):
     name = 'colg'
@@ -7,15 +9,27 @@ class DjSpider(Spider):
         'https://bbs.colg.cn/forum-171-1.html',
     ]
 
+    starter_url = 'https://bbs.colg.cn/'
 
-
+    # first page
     def parse(self, response):
-        items = []
-        for each in response.xpath("//a[@class='s xst']/@href"):
-            item = ScrcolgItem()
-            header = each.extract()
-            print(header)
+        # parse the page and yield items
+        next_page = response.xpath("//a[@class='nxt']/@href").extract()[0]
+        url_next_page = self.starter_url+next_page
+        print(url_next_page)
+        yield scrapy.Request(url_next_page, self.parse)
+        #
+        # for each in response.xpath("//a[@class='s xst']/@href"):
+        #     header = each.extract()
+        #     print(header)
 
-        print(items)
-        print(len(items))
+
+    # def parsecontent(self, response):
+    #     # parse the page and yield items
+    #     ____next_page = response.xpath("some link element for the next page")
+    #     ____if
+    #     next_page is not None:
+    #     ________url_next_page = next_page.get_url()
+    #     ________yield
+    #     scrapy.Request(url_next_page, self.parse)
 
