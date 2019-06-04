cc75979a39cbbaf5963db16e7c96597c4942ae3b
wending <wending.lin@unity3d.com>
2019-06-04 15:05:18 +0800

    add first scan

--- scrcolg/scrcolg/spiders/test.py
+++ scrcolg/scrcolg/spiders/test.py
@@ -3,6 +3,7 @@ from scrapy.spiders import Spider
 
 from ..items import topic, reply
 
+
 class DjSpider(Spider):
     name = 'colg'
     start_urls = [
@@ -10,18 +11,26 @@ class DjSpider(Spider):
     ]
 
     starter_url = 'https://bbs.colg.cn/'
+    page_counter = 0
 
     # first page
     def parse(self, response):
+        for each in response.xpath("//table[@id='threadlisttableid']/tbody[contains(@id,'normal')]"):
+            type = each.xpath("tr/th/em/a/text()").extract_first()
+            content = each.xpath("tr/th/a[1]/text()").extract_first()
+            author = each.xpath("tr/td[2]/cite/a/text()").extract_first()
+            date = each.xpath("tr/td[2]/em/span/text()").extract_first()
+            print("happy")
+
         # parse the page and yield items
+        if self.page_counter == 1:
+            return
         next_page = response.xpath("//a[@class='nxt']/@href").extract()[0]
-        url_next_page = self.starter_url+next_page
+        url_next_page = self.starter_url + next_page
         print(url_next_page)
+        self.page_counter += 1
         yield scrapy.Request(url_next_page, self.parse)
-        #
-        # for each in response.xpath("//a[@class='s xst']/@href"):
-        #     header = each.extract()
-        #     print(header)
+
 
 
     # def parsecontent(self, response):
@@ -32,4 +41,3 @@ class DjSpider(Spider):
     #     ________url_next_page = next_page.get_url()
     #     ________yield
     #     scrapy.Request(url_next_page, self.parse)
-
